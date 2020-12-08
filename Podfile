b6c8008af4921946f36a7a73470301d95c870fd1
Gustavo Tiecker <gustavotiecker@gmail.com>
2020-12-08 18:56:52 -0300

    adding Alamofire

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '10.0'
+
+target 'SpaceXInfo' do
+  use_frameworks!
+
+  # Pods for SpaceXInfo
+  
+  pod 'Alamofire', '~> 5.2'
+
+end
---
+++ Pods/Target Support Files/Pods-SpaceXInfo/Pods-SpaceXInfo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SpaceXInfo : NSObject
+@end
+@implementation PodsDummy_Pods_SpaceXInfo
+@end
